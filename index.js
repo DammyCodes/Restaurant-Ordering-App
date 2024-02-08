86e88bf68c8deb2fdb96d7ab3e98a40cf8c1000f
Damilola Okunlola <okunlolatawakalitu61@gmail.com>
2024-02-08 01:26:01 +0100

    modified index.js and styles.css files

--- index.js
+++ index.js
@@ -2,25 +2,36 @@ import { menuArray } from './data.js'
 
 const addBtn = document.getElementById('add-btn')
 
-document.addEventListener('click', function (e) {
+document.addEventListener('click', function(e) {
     getMyOrder(e.target.dataset.add)
-  
+    // updateOrderList(e.target.dataset.add)
+    
     
 })
 
-let orderHtml = ``
-function getMyOrder(itemId) {
+// function updateOrderList(itemId) {
+//     const targetMenuObj = menuArray.filter(function(menu) {
+//         if (itemId === menu.id) {
+//             targetMenuObj.push(menu)
+//         }
+        
+//     })
+   
+//     console.log(targetMenuObj)
+// }
 
+let orderHtml = ``
+
+function getMyOrder(itemId) {
     menuArray.map(function(menu) {
-        if (itemId === `${menu.id}`) {
-            
+       if (itemId === menu.id) {
             orderHtml =  ` 
         <div class="order-wrapper">
         <h2>Your order</h2>
-            <div class="order-item underline">
-                <h2>${menu.name}<span class="remove">remove</span></h2>
-                <div class="order-price">$${menu.price}</div>
-            </div>
+        <div class="order-item underline" id="order-item">
+            <h2>${menu.name}<span class="remove">remove</span></h2>
+            <div class="order-price">$${menu.price}</div>
+        </div>
 
             <div class="order-item total-price">
                 <h2>Total price:</h2>
@@ -67,6 +78,3 @@ function render() {
 }
 
 render()
-
-
-
