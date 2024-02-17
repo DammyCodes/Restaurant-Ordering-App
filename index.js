b6e0c25de640c863f95d7ccd22e603433d43f535
Damilola Okunlola <okunlolatawakalitu61@gmail.com>
2024-02-17 19:12:51 +0100

    updated index.html, index.js and style.css files

--- index.js
+++ index.js
@@ -1,80 +1,119 @@
 import { menuArray } from './data.js'
 
-const addBtn = document.getElementById('add-btn')
+let orderArr = []
+const orderBtn = document.querySelector('.order-btn')
+const pay = document.querySelector('.submit')
+const formData = document.getElementById('form-data')
+const orderSummary = document.getElementById('order-summary')
+const orderArrCont = document.querySelector('.order-array-cont')
 
+
+
+// this is a function used to render other functions when an element is clicked
 document.addEventListener('click', function(e) {
-    getMyOrder(e.target.dataset.add)
-    // updateOrderList(e.target.dataset.add)
-    
-    
+    if (e.target.dataset.add) {
+        document.querySelector('.order-wrapper').style.display = 'flex';
+        getArr(e.target.dataset.add);
+        getOrderArray();
+    }
+    if (e.target.dataset.remove) {
+        getArr(e.target.dataset.add)
+        deleteArr(e.target.dataset.remove)
+    }
 })
 
-// function updateOrderList(itemId) {
-//     const targetMenuObj = menuArray.filter(function(menu) {
-//         if (itemId === menu.id) {
-//             targetMenuObj.push(menu)
-//         }
-        
-//     })
-   
-//     console.log(targetMenuObj)
-// }
 
-let orderHtml = ``
+orderBtn.addEventListener('click', function() {
+    document.querySelector('.card-details').style.display = 'flex'
+}) 
+  
+pay.addEventListener('click', function() {
+    payforOrder(e)
+})  
 
-function getMyOrder(itemId) {
-    menuArray.map(function(menu) {
-       if (itemId === menu.id) {
-            orderHtml =  ` 
-        <div class="order-wrapper">
-        <h2>Your order</h2>
-        <div class="order-item underline" id="order-item">
-            <h2>${menu.name}<span class="remove">remove</span></h2>
-            <div class="order-price">$${menu.price}</div>
-        </div>
+// function to render the items on the menu
+document.getElementById('menu-container').innerHTML = menuArray.map(function(menu) {
+    const {emoji, name, ingredients, price, id} = menu
 
-            <div class="order-item total-price">
-                <h2>Total price:</h2>
-                <div class="order-price">$${menu.price}</div>
+    return(
+    `
+    <div class="menu-list">
+        <div class="menu-item">
+            <img src="${emoji}">
+            <div class="menu-info">
+                <h2>${name}</h2>
+                <p>${ingredients}</p>
+                <h2>$${price}</h2>
             </div>
-            <button class="order-btn">Complete order</button>
         </div>
-     `
-        }
         
-    })
-   
-   document.querySelector('.order-container').innerHTML = orderHtml
-}
+        <div id="add-btn" data-add="${id}">+</div>
+    </div>  
+    `
+    )
+})
 
-
-
-
-function getMenuArray() {
-    let menuHtml = ``
+// function that stores item from the displayed menu
+function getArr(itemId) {
     menuArray.forEach(function(menu) {
-
-        menuHtml += `
-        <div class="menu-list">
-            <div class="menu-item">
-                <img src="${menu.emoji}">
-                <div class="menu-info">
-                    <h2>${menu.name}</h2>
-                    <p>${menu.ingredients}</p>
-                    <h2>${menu.price}</h2>
-                </div>
-            </div>
-            
-            <div id="add-btn" data-add="${menu.id}">+</div>
-        </div>  
-        `
+        if (Number(itemId) === menu.id) {
+            orderArr.push(menu)
+        }
     })
-    return menuHtml
+   
 }
 
 
-function render() {
-    document.querySelector('.menu-container').innerHTML = getMenuArray()
+
+// function that renders the items ordered
+function getOrderArray() {
+    let arrayHtml = ''
+    let totalPrice = 0
+
+    orderArr.forEach(function(menu) {
+        const {name, price, id} = menu
+        arrayHtml +=  `
+        <div class="ordered-array">
+            <h2>${name} 
+            <span class="remove" data-remove="${id}">remove</span></h2>
+            <p class="order-price">$${price}</p>
+        </div>
+        `
+        totalPrice += menu.price
+       
+    })
+  
+    document.getElementById('total-amount').textContent = `$${totalPrice}`
+    document.getElementById('order-item').innerHTML = arrayHtml
+
+       
 }
 
-render()
+function deleteArr(itemId) {
+    let newOrderArr = orderArr.filter(function(menu) {
+          return Number(itemId) !== menu.id
+     })
+     orderArr = newOrderArr
+    getOrderArray()
+    
+  }
+  
+
+function payforOrder(e) {
+    e.preventDefault()
+    const cardDetailsData = new FormData(formData)
+    const fullName = cardDetailsData.get('fullName')
+    document.querySelector('.card-details').style.display = 'none'
+    orderSummary.style.display = 'flex'
+    let newDisplay = ``
+    newDisplay += 
+    ` <div class="my-order">
+        Hi ${fullName}, your order is on the way!
+    </div>
+    `
+    orderArrCont.innerHTML = newDisplay 
+    console.log(orderArrCont)
+}
+   
+
+
